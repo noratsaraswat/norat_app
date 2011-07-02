010c8a7c2834184dc73c5576fc7e46ceb349883a
norat S saraswat <noratsaraswat1@yahoo.com>
2011-07-02 11:19:41 +0530

    my second apdddddp

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+ 
+  # render new.rhtml
+  def new
+    @user = User.new
+  end
+
+  def create
+    logout_keeping_session!
+    puts "---------------------------------"
+    puts params.inspect
+    puts "---------------------------------"
+    @user = User.new(params[:user])
+    success = @user && @user.save
+    if success && @user.errors.empty?
+            # Protects against session fixation attacks, causes request forgery
+      # protection if visitor resubmits an earlier form using back
+      # button. Uncomment if you understand the tradeoffs.
+      # reset session
+      self.current_user = @user # !! now logged in
+      redirect_back_or_default('/')
+      flash[:notice] = "Thanks for signing up!"
+    else
+      flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
+      render :action => 'new'
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:notice] = "Successfully updated user."
+      redirect_to @user
+    else
+      render :action => 'edit'
+    end
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+end
---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'dbdf74db6dafe60b128917377c26fab50d000a98fa699bd433f5f109e78ac664dad41aa36fcdf09067f64670c7f394f6c9474255f8bc888cdcffe476138fd0fd';
